2fc59157f84405f2305c18e2450836f693568005
AnushaTejeswari <83825729+AnushaTejeswari@users.noreply.github.com>
2022-04-07 11:15:25 +0530

    commit

--- code-compiler/server/index.js
+++ code-compiler/server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const Axios = require("axios");
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
