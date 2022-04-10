7f75eca1432540d051b8479831ec5ec50f07e6e6
Sravani Siripurapu <sravanisiripurapu888@gmail.com>
2022-04-10 16:32:16 +0530

    User authentication added

--- src/app.js
+++ src/app.js
@@ -1,19 +1,70 @@
-let express = require( 'express' );
-let app = express();
-let server = require( 'http' ).Server( app );
-let io = require( 'socket.io' )( server );
-let stream = require( './ws/stream' );
-let path = require( 'path' );
-let favicon = require( 'serve-favicon' );
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const stream = require('./ws/stream');
+const path = require('path');
+const favicon = require('serve-favicon');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-app.use( favicon( path.join( __dirname, 'favicon.ico' ) ) );
-app.use( '/assets', express.static( path.join( __dirname, 'assets' ) ) );
+mongoose.connect('mongodb+srv://Anusha:anusha123@cluster0.sllbp.mongodb.net/myusers?retryWrites=true&w=majority', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
+const db = mongoose.connection;
+db.on('open', () => console.log('Connected to Database!'));
+db.on('error', () => console.log('Error in connecting to Database.'));
 
-app.get( '/', ( req, res ) => {
-    res.sendFile( __dirname + '/index.html' );
-} );
+app.use(favicon(path.join(__dirname, 'favicon.ico')));
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-io.of( '/stream' ).on( 'connection', stream );
+const UserSchema = mongoose.Schema({
+	name: String,
+	email: String,
+	phone: String,
+	password: String,
+	userType: String
+});
+const User = new mongoose.model('User', UserSchema);
 
-server.listen( 3000 );
+// User register API
+app.post('/signup', (req, res) => {
+	var data = req.body;
+	db.collection('users').insertOne(data, (err, result) => {
+		if (err) throw err;
+		delete data.password;
+		delete data._id;
+		return res.status(200).json({ message: 'User added successfully', user: data, status: 'success' });
+	});
+});
+
+// User Login API
+app.post('/login', (req, res) => {
+	var data = req.body;
+	User.findOne(data, (err, result) => {
+		if (err) throw err;
+		if (result) {
+			return res.status(200).json({
+				user: { name: result.name, email: result.email, phone: result.phone, userType: result.userType },
+				status: 'success'
+			});
+		} else {
+			res.status(403).json({ message: 'Invalid username or password', status: 'error' });
+		}
+	});
+});
+
+app.get('/', (req, res) => {
+	res.set({
+		'Allow-access-Allow-Origin': '*'
+	});
+	res.sendFile(__dirname + '/index.html');
+});
+
+io.of('/stream').on('connection', stream);
+
+server.listen(3000);
--- src/assets/js/events.js
+++ src/assets/js/events.js
@@ -1,121 +1,198 @@
 import helpers from './helpers.js';
 
-window.addEventListener( 'load', () => {
-    //When the chat icon is clicked
-    document.querySelector( '#toggle-chat-pane' ).addEventListener( 'click', ( e ) => {
-        let chatElem = document.querySelector( '#chat-pane' );
-        let mainSecElem = document.querySelector( '#main-section' );
+window.addEventListener('load', () => {
+	//When the chat icon is clicked
+	document.querySelector('#toggle-chat-pane').addEventListener('click', (e) => {
+		let chatElem = document.querySelector('#chat-pane');
+		let mainSecElem = document.querySelector('#main-section');
 
-        if ( chatElem.classList.contains( 'chat-opened' ) ) {
-            chatElem.setAttribute( 'hidden', true );
-            mainSecElem.classList.remove( 'col-md-9' );
-            mainSecElem.classList.add( 'col-md-12' );
-            chatElem.classList.remove( 'chat-opened' );
-        }
+		if (chatElem.classList.contains('chat-opened')) {
+			chatElem.setAttribute('hidden', true);
+			mainSecElem.classList.remove('col-md-9');
+			mainSecElem.classList.add('col-md-12');
+			chatElem.classList.remove('chat-opened');
+		} else {
+			chatElem.attributes.removeNamedItem('hidden');
+			mainSecElem.classList.remove('col-md-12');
+			mainSecElem.classList.add('col-md-9');
+			chatElem.classList.add('chat-opened');
+		}
 
-        else {
-            chatElem.attributes.removeNamedItem( 'hidden' );
-            mainSecElem.classList.remove( 'col-md-12' );
-            mainSecElem.classList.add( 'col-md-9' );
-            chatElem.classList.add( 'chat-opened' );
-        }
+		//remove the 'New' badge on chat icon (if any) once chat is opened.
+		setTimeout(() => {
+			if (document.querySelector('#chat-pane').classList.contains('chat-opened')) {
+				helpers.toggleChatNotificationBadge();
+			}
+		}, 300);
+	});
 
-        //remove the 'New' badge on chat icon (if any) once chat is opened.
-        setTimeout( () => {
-            if ( document.querySelector( '#chat-pane' ).classList.contains( 'chat-opened' ) ) {
-                helpers.toggleChatNotificationBadge();
-            }
-        }, 300 );
-    } );
+	//When the video frame is clicked. This will enable picture-in-picture
+	document.getElementById('local').addEventListener('click', () => {
+		if (!document.pictureInPictureElement) {
+			document.getElementById('local').requestPictureInPicture().catch((error) => {
+				// Video failed to enter Picture-in-Picture mode.
+				console.error(error);
+			});
+		} else {
+			document.exitPictureInPicture().catch((error) => {
+				// Video failed to leave Picture-in-Picture mode.
+				console.error(error);
+			});
+		}
+	});
 
+	// When the login text is clicked
+	document.getElementById('login-btn').addEventListener('click', () => {
+		document.querySelector('#user-signup').setAttribute('hidden', true);
+		document.querySelector('#user-login').attributes.removeNamedItem('hidden');
+	});
 
-    //When the video frame is clicked. This will enable picture-in-picture
-    document.getElementById( 'local' ).addEventListener( 'click', () => {
-        if ( !document.pictureInPictureElement ) {
-            document.getElementById( 'local' ).requestPictureInPicture()
-                .catch( error => {
-                    // Video failed to enter Picture-in-Picture mode.
-                    console.error( error );
-                } );
-        }
+	// When the register text is clicked
+	document.getElementById('register-btn').addEventListener('click', () => {
+		document.querySelector('#user-signup').attributes.removeNamedItem('hidden');
+		document.querySelector('#user-login').setAttribute('hidden', true);
+	});
 
-        else {
-            document.exitPictureInPicture()
-                .catch( error => {
-                    // Video failed to leave Picture-in-Picture mode.
-                    console.error( error );
-                } );
-        }
-    } );
+	// When the 'Sign Up' button is clicked
+	document.getElementById('signup').addEventListener('click', () => {
+		let form = document.forms['signUpForm'];
+		let name = form['name'].value;
+		let email = form['email'].value;
+		let phone = form['phone'].value;
+		let password = form['password'].value;
+		let userType = form['userType'].value;
+		if (name && email && phone && password) {
+			let data = { name, email, phone, password, userType };
+			fetch('/signup', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(data)
+			})
+				.then((response) => response.json())
+				.then((result) => {
+					let user = result.user;
+					console.log('Success:', user);
+					localStorage.setItem('user', JSON.stringify(user));
 
+					if (user && user.userType == 'faculty') {
+						document.querySelector('#room-create').attributes.removeNamedItem('hidden');
+					} else {
+						document.querySelector('#enter-room-url').attributes.removeNamedItem('hidden');
+					}
 
-    //When the 'Create room" is button is clicked
-    document.getElementById( 'create-room' ).addEventListener( 'click', ( e ) => {
-        e.preventDefault();
+					document.querySelector('#user-signup').setAttribute('hidden', true);
+					document.querySelector('#user-login').setAttribute('hidden', true);
+				})
+				.catch((error) => {
+					console.error('Error:', error);
+				});
+		}
+	});
 
-        let roomName = document.querySelector( '#room-name' ).value;
-        let yourName = document.querySelector( '#your-name' ).value;
+	// When the 'Login' button is clicked
+	document.getElementById('login').addEventListener('click', () => {
+		let form = document.forms['loginForm'];
+		let email = form['email'].value;
+		let password = form['password'].value;
+		if (email && password) {
+			let data = { email, password };
+			fetch('/login', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(data)
+			})
+				.then((response) => response.json())
+				.then((result) => {
+					if (result.status == 'success') {
+						let user = result.user;
+						console.log('Success:', user);
 
-        if ( roomName && yourName ) {
-            //remove error message, if any
-            document.querySelector('#err-msg').innerText = "";
+						localStorage.setItem('user', JSON.stringify(user));
 
-            //save the user's name in sessionStorage
-            sessionStorage.setItem( 'username', yourName );
+						if (user && user.userType == 'faculty') {
+							document.querySelector('#room-create').attributes.removeNamedItem('hidden');
+						} else {
+							document.querySelector('#enter-room-url').attributes.removeNamedItem('hidden');
+						}
 
-            //create room link
-            let roomLink = `${ location.origin }?room=${ roomName.trim().replace( ' ', '_' ) }_${ helpers.generateRandomString() }`;
+						document.querySelector('#user-signup').setAttribute('hidden', true);
+						document.querySelector('#user-login').setAttribute('hidden', true);
+					}
+				})
+				.catch((error) => {
+					console.error('Error:', error);
+				});
+		}
+	});
 
-            //show message with link to room
-            document.querySelector( '#room-created' ).innerHTML = `Room successfully created. Click <a href='${ roomLink }'>here</a> to enter room. 
+	//When the 'Create room" is button is clicked
+	document.getElementById('create-room').addEventListener('click', (e) => {
+		e.preventDefault();
+
+		let user = localStorage.getItem('user');
+		user = user ? JSON.parse(user) : {};
+
+		let roomName = document.querySelector('#room-name').value;
+		let yourName = user.name;
+
+		if (roomName && yourName) {
+			//remove error message, if any
+			document.querySelector('#err-msg').innerText = '';
+
+			//create room link
+			let roomLink = `${location.origin}?room=${roomName
+				.trim()
+				.replace(' ', '_')}_${helpers.generateRandomString()}`;
+
+			//show message with link to room
+			document.querySelector(
+				'#room-created'
+			).innerHTML = `Room successfully created. Click <a href='${roomLink}'>here</a> to enter room. 
                 Share the room link with your partners.`;
 
-            //empty the values
-            document.querySelector( '#room-name' ).value = '';
-            document.querySelector( '#your-name' ).value = '';
-        }
+			//empty the values
+			document.querySelector('#room-name').value = '';
+			document.querySelector('#your-name').value = '';
+		} else {
+			document.querySelector('#err-msg').innerText = 'All fields are required';
+		}
+	});
 
-        else {
-            document.querySelector('#err-msg').innerText = "All fields are required";
-        }
-    } );
+	//When the 'Enter room' button is clicked.
+	document.getElementById('enter-room').addEventListener('click', (e) => {
+		e.preventDefault();
 
+		let roomId = document.querySelector('#roomId').value;
 
-    //When the 'Enter room' button is clicked.
-    document.getElementById( 'enter-room' ).addEventListener( 'click', ( e ) => {
-        e.preventDefault();
+		if (roomId) {
+			//remove error message, if any
+			document.querySelector('#err-msg-username').innerText = '';
 
-        let name = document.querySelector( '#username' ).value;
+			//load room
+			window.open(location.origin + '/?room=' + roomId, '_self');
+		} else {
+			document.querySelector('#err-msg-username').innerText = 'Please input room url';
+		}
+	});
 
-        if ( name ) {
-            //remove error message, if any
-            document.querySelector('#err-msg-username').innerText = "";
+	document.addEventListener('click', (e) => {
+		if (e.target && e.target.classList.contains('expand-remote-video')) {
+			helpers.maximiseStream(e);
+		} else if (e.target && e.target.classList.contains('mute-remote-mic')) {
+			helpers.singleStreamToggleMute(e);
+		}
+	});
 
-            //save the user's name in sessionStorage
-            sessionStorage.setItem( 'username', name );
+	document.getElementById('closeModal').addEventListener('click', () => {
+		helpers.toggleModal('recording-options-modal', false);
+	});
 
-            //reload room
-            location.reload();
-        }
-
-        else {
-            document.querySelector('#err-msg-username').innerText = "Please input your name";
-        }
-    } );
-
-
-    document.addEventListener( 'click', ( e ) => {
-        if ( e.target && e.target.classList.contains( 'expand-remote-video' ) ) {
-            helpers.maximiseStream( e );
-        }
-
-        else if ( e.target && e.target.classList.contains( 'mute-remote-mic' ) ) {
-            helpers.singleStreamToggleMute( e );
-        }
-    } );
-
-
-    document.getElementById( 'closeModal' ).addEventListener( 'click', () => {
-        helpers.toggleModal( 'recording-options-modal', false );
-    } );
-} );
+	document.getElementById('logout').addEventListener('click', () => {
+		localStorage.removeItem('user');
+		location.reload();
+	});
+});
